ab13d049c71557c4c72dfad6aa964052ba465e4e
Dorian <dguillet6@gmail.com>
2025-01-15 23:10:23 +0100

    update filename

--- Bowen.lean
+++ Bowen.lean
@@ -1,4 +1,4 @@
 -- This module serves as the root of the `Bowen` library.
 -- Import modules here that should be built as part of the library.
 import Bowen.Basic
-import Bowen.Def
+import Bowen.Bernoulli
