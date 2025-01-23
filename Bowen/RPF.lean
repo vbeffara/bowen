da32ccee50973cc5c2b122beef0997f185e052d3
Dorian <dguillet6@gmail.com>
2025-01-23 12:17:10 +0100

    add Schauder-Tychonoff theorem statement

--- Bowen/RPF.lean
+++ Bowen/RPF.lean
@@ -1,12 +1,21 @@
 import Mathlib
 
-open Set Real
+open Set Real MeasureTheory
+
+
+/-- Schauder-Tychonoff Theorem: A compact convex subset of a locally convex linear
+topological space has the fixed point property. -/
+theorem schauder_tychonoff
+  {E : Type*} [TopologicalSpace E] [AddCommGroup E] [Module ℝ E]
+  [TopologicalAddGroup E] [ContinuousSMul ℝ E] [LocallyConvexSpace ℝ E]
+  {K : Set E} (hK : IsCompact K) (hK_convex : Convex ℝ K) (f : E → E) :
+  ContinuousOn f K ∧ (f '' K ⊆ K) → ∃ x ∈ K, f x = x := sorry
 
 namespace RPF
 
   abbrev PBernoulli (n : ℕ) := ℕ → Fin n
 
-  variable {n : ℕ} {x y z : PBernoulli n}
+  variable {n : ℕ} {x y z : PBernoulli n} {φ : PBernoulli n → ℝ} {b : NNReal} {α : Ioo 0 1}
 
   instance : MetricSpace (Fin n) where
     dist x y := if x = y then 0 else 1
@@ -27,8 +36,8 @@ namespace RPF
 
   def cylinder (x : PBernoulli n) (k : ℕ) : Set (PBernoulli n) := {y | ∀ i ∈ Ico 0 k, x i = y i}
 
-  structure Holder (φ : PBernoulli n → ℝ) where
-    isHolder : ∃ b : NNReal, ∃ α ∈ Ioo 0 1, ∀ x y : PBernoulli n, ∀ k : ℕ,
+  structure Holder (φ : PBernoulli n → ℝ) (b : NNReal) (α : Ioo 0 1) where
+    isHolder : ∀ x y : PBernoulli n, ∀ k : ℕ,
       y ∈ cylinder x k → |φ x - φ y| ≤ b * α ^ k
 
   def shift (x : PBernoulli n) : PBernoulli n := fun i => x (i + 1)
@@ -37,5 +46,17 @@ namespace RPF
 
   noncomputable def transfert (φ : PBernoulli n → ℝ) (f : PBernoulli n → ℝ) : PBernoulli n → ℝ :=
     fun x => ∑ y ∈ preimage shift {x}, f y * exp (φ y)
+
+  theorem im_transfert_continuous (hφ : Continuous φ)
+    : Continuous (transfert φ)
+    := by sorry
+
+  -- noncomputable def transfert_measure (φ : PBernoulli n → ℝ) (μ : Measure (PBernoulli n)) :=
+
+  variable {hφ : Holder φ b α}
+
+  -- theorem rpf1 (hφ : Holder φ b α)
+
+  -- noncomputable def B (m : ℕ) := exp (∑' k ∈ {k : ℕ | k > m}, 2 * b * α^k)
 
 end RPF
