f0c0f84d36d9bcae3a15809e0d4a14f3aa326075
Dorian <dguillet6@gmail.com>
2025-03-09 21:33:20 +0100

    Finish proof of equivalent relation on balls

--- Bowen/Ultrametric.lean
+++ Bowen/Ultrametric.lean
@@ -29,38 +29,58 @@ lemma open_eq_union_ball {X : Type*} [PseudoMetricSpace X] (O : Set X) (hO : IsO
     ∃ s ⊆ {b : Set X | ∃ x r, 0 < r ∧ b = ball x r}, O = ⋃₀ s := by
       exact IsTopologicalBasis.open_eq_sUnion metric_space_topological_basis hO
 
-def rel (u v : Set X) : Prop :=
-  ∃ w ∈ {b : Set X | ∃ x r, 0 < r ∧ b = ball x r}, u ⊆ w ∧ v ⊆ w
+def rel (U : Set (Set X)) (u v : Set X) : Prop :=
+  ∃ w ∈ {b : Set X | ∃ x r, 0 < r ∧ b = ball x r} ∩ U, u ⊆ w ∧ v ⊆ w
 
-instance : IsEquiv {b : Set X | ∃ x r, 0 < r ∧ b = ball x r} (fun ⟨u, hu⟩ ⟨v, hv⟩ => rel u v) where
+instance (U : Set (Set X)) :
+    IsEquiv {b : Set X | ∃ x r, 0 < r ∧ b = ball x r ∧ b ∈ U} (fun ⟨u, _⟩ ⟨v, _⟩ => rel U u v) where
   refl u := by
-    obtain ⟨u', hu⟩ := u
+    obtain ⟨u, xu, ru, ru_pos, u_ball, u_in_U⟩ := u
     simp_all only [rel, mem_setOf_eq, and_self]
-    use u'
-    simp_all only [mem_setOf_eq, subset_refl, and_self]
+    use u
+    simp_all only [mem_inter_iff, mem_setOf_eq, subset_refl, and_true]
+    exact ⟨xu, ⟨ru, ru_pos, rfl⟩⟩
   symm u v := by
     intro huv
     simp_all only [rel]
     obtain ⟨w, hwU, huw, hvw⟩ := huv
     exact ⟨w, hwU, hvw, huw⟩
   trans u v w := by
-    obtain ⟨u, xu, ru, ru_pos, u_ball⟩ := u
-    obtain ⟨v, xv, rv, rv_pos, v_ball⟩ := v
-    obtain ⟨w, xw, rw, rw_pos, w_ball⟩ := w
+    obtain ⟨u, xu, ru, ru_pos, u_ball, u_in_U⟩ := u
+    obtain ⟨v, xv, rv, rv_pos, v_ball, v_in_U⟩ := v
+    obtain ⟨w, xw, rw, rw_pos, w_ball, w_in_U⟩ := w
     intros huv hvw
-    simp_all only [rel]
-    obtain ⟨s, ⟨xs, rs, rs_pos, s_ball⟩, u_sub_s, v_sub_s⟩ := huv
-    obtain ⟨t, ⟨xt, rt, rt_pos, t_ball⟩, v_sub_t, w_sub_t⟩ := hvw
-    simp_all
+    simp_all only [rel, mem_inter_iff]
+    obtain ⟨s, ⟨⟨xs, rs, rs_pos, s_ball⟩, hsU⟩, u_sub_s, v_sub_s⟩ := huv
+    obtain ⟨t, ⟨⟨xt, rt, rt_pos, t_ball⟩, htU⟩, v_sub_t, w_sub_t⟩ := hvw
     have v_sub_inter : v ⊆ s ∩ t := by simp_all only [subset_inter_iff, and_self]
+    have v_nonempty : v ≠ ∅ := by simp_all
+    have inter_nonempty : s ∩ t ≠ ∅ := by
+      intro is_empty
+      exact v_nonempty (subset_eq_empty v_sub_inter is_empty)
     have s_sub_or_t_sub : s ⊆ t ∨ t ⊆ s := by
       rw [s_ball, t_ball]
       rcases IsUltrametricDist.ball_subset_trichotomy xs xt rs rt with s_sub | t_sub | dis
       . left; exact s_sub
       . right; exact t_sub
-      . sorry
-    sorry
-  -- FIX : Peut-etre changer U par l'ensemble des boules pour avoir trans
+      . rw [← s_ball, ← t_ball, disjoint_iff_inter_eq_empty] at dis
+        simp at inter_nonempty
+        exfalso
+        exact inter_nonempty dis
+
+    rcases s_sub_or_t_sub with s_sub | t_sub
+    . use t
+      split_ands
+      . exact ⟨xt, rt, rt_pos, t_ball⟩
+      . exact htU
+      . exact subset_trans u_sub_s s_sub
+      . exact w_sub_t
+    . use s
+      split_ands
+      . exact ⟨xs, rs, rs_pos, s_ball⟩
+      . exact hsU
+      . exact u_sub_s
+      . exact subset_trans w_sub_t t_sub
 
 theorem open_eq_disjoint_union_ball (O : Set X) (hO : IsOpen O) :
     ∃ s ⊆ {b : Set X | ∃ x r, b = ball x r}, O = ⋃₀ s ∧ s.PairwiseDisjoint id := by
