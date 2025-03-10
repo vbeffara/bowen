190042997d804f8fcca0f1f46bbccb108aa02f2c
Vincent Beffara <vbeffara@gmail.com>
2025-03-10 21:43:47 +0100

    Golf

--- Bowen/Ultrametric.lean
+++ Bowen/Ultrametric.lean
@@ -27,53 +27,33 @@ def rel (U : Set (Set X)) (u v : Set X) : Prop := ∃ w ∈ balls X ∩ U, u ⊆
 lemma rel_equiv (U : Set (Set X)) :
     Equivalence (fun (u v : {b ∈ balls X | b ∈ U}) => rel U u v) where
   refl s := ⟨s, by simp⟩
-  symm := by
-    intros u v huv
-    simp_all only [rel]
+  symm {u v} huv := by
     obtain ⟨w, hwU, huw, hvw⟩ := huv
     exact ⟨w, hwU, hvw, huw⟩
   trans := by
-    rintro ⟨u, ⟨⟨xu, ru, ru_pos, u_ball⟩, u_in_U⟩⟩
-    rintro ⟨v, ⟨⟨xv, rv, rv_pos, v_ball⟩, v_in_U⟩⟩
-    rintro ⟨w, ⟨⟨xw, rw, rw_pos, w_ball⟩, w_in_U⟩⟩
-    intros huv hvw
-    simp_all only [rel, mem_inter_iff]
-    obtain ⟨s, ⟨⟨xs, rs, rs_pos, s_ball⟩, hsU⟩, u_sub_s, v_sub_s⟩ := huv
-    obtain ⟨t, ⟨⟨xt, rt, rt_pos, t_ball⟩, htU⟩, v_sub_t, w_sub_t⟩ := hvw
-    have v_sub_inter : v ⊆ s ∩ t := by simp_all only [subset_inter_iff, and_self]
-    have v_nonempty : v ≠ ∅ := by simp_all
-    have inter_nonempty : s ∩ t ≠ ∅ := by
-      intro is_empty
-      exact v_nonempty (subset_eq_empty v_sub_inter is_empty)
-    have s_sub_or_t_sub : s ⊆ t ∨ t ⊆ s := by
-      rw [s_ball, t_ball]
-      rcases IsUltrametricDist.ball_subset_trichotomy xs xt rs rt with s_sub | t_sub | dis
-      . left; exact s_sub
-      . right; exact t_sub
-      . rw [← s_ball, ← t_ball, disjoint_iff_inter_eq_empty] at dis
-        simp at inter_nonempty
-        exfalso
-        exact inter_nonempty dis
+    rintro ⟨u, ⟨⟨xu, ru, ru_pos, rfl⟩, u_in_U⟩⟩
+    rintro ⟨v, ⟨⟨xv, rv, rv_pos, rfl⟩, v_in_U⟩⟩
+    rintro ⟨w, ⟨⟨xw, rw, rw_pos, rfl⟩, w_in_U⟩⟩
+    rintro ⟨s, ⟨⟨xs, rs, rs_pos, rfl⟩, hsU⟩, u_sub_s, v_sub_s⟩
+    rintro ⟨t, ⟨⟨xt, rt, rt_pos, rfl⟩, htU⟩, v_sub_t, w_sub_t⟩
 
-    rcases s_sub_or_t_sub with s_sub | t_sub
-    . use t
-      split_ands
-      . exact ⟨xt, rt, rt_pos, t_ball⟩
-      . exact htU
-      . exact subset_trans u_sub_s s_sub
-      . exact w_sub_t
-    . use s
-      split_ands
-      . exact ⟨xs, rs, rs_pos, s_ball⟩
-      . exact hsU
-      . exact u_sub_s
-      . exact subset_trans w_sub_t t_sub
+    have v_sub_inter : ball xv rv ⊆ ball xs rs ∩ ball xt rt := by
+      intro z hz
+      exact ⟨v_sub_s hz, v_sub_t hz⟩
+    have inter_nonempty : Set.Nonempty (ball xs rs ∩ ball xt rt) := by
+      refine ⟨xv, v_sub_s (mem_ball_self rv_pos), v_sub_t (mem_ball_self rv_pos)⟩
+
+    rcases IsUltrametricDist.ball_subset_trichotomy xs xt rs rt with s_sub | t_sub | dis
+    · exact ⟨ball xt rt, ⟨⟨xt, rt, rt_pos, rfl⟩, htU⟩, subset_trans u_sub_s s_sub, w_sub_t⟩
+    · exact ⟨ball xs rs, ⟨⟨xs, rs, rs_pos, rfl⟩, hsU⟩, u_sub_s, subset_trans w_sub_t t_sub⟩
+    · rw [disjoint_iff_inter_eq_empty] at dis
+      simp [dis] at inter_nonempty
 
 instance balls_U (U : Set (Set X)) : Setoid {b ∈ balls X | b ∈ U} where
   r := fun u v => rel U u v
   iseqv := rel_equiv U
 
-def Urepr (U : Set (Set X)) : Set (Set X) := {x | ∃ (q : Quotient (balls_U U)), Quotient.out q = x}
+-- def Urepr (U : Set (Set X)) : Set (Set X) := {x | ∃ (q : Quotient (balls_U U)), Quotient.out q = x}
 
 lemma has_max (U : Set (Set X)) (u : Quotient (balls_U U)) :
     ∃ v : {b ∈ balls X | b ∈ U}, Maximal (fun w => ⟦w⟧ = u) v := sorry
@@ -81,5 +61,5 @@ lemma has_max (U : Set (Set X)) (u : Quotient (balls_U U)) :
   -- Piste : Lemme de Zorn
 
 theorem open_eq_disjoint_union_ball (O : Set X) (hO : IsOpen O) :
-    ∃ s ⊆ {b : Set X | ∃ x r, b = ball x r}, O = ⋃₀ s ∧ s.PairwiseDisjoint id := by
+    ∃ s ⊆ balls X, O = ⋃₀ s ∧ s.PairwiseDisjoint id := by
   sorry
