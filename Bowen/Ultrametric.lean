159a7c5a4b2da074abec46ae7b3dc619483c0f3f
Vincent Beffara <vbeffara@gmail.com>
2025-03-10 23:17:37 +0100

    Golf

--- Bowen/Ultrametric.lean
+++ Bowen/Ultrametric.lean
@@ -4,7 +4,7 @@ open TopologicalSpace Metric Set Classical
 
 variable {X : Type*} [PseudoMetricSpace X] [IsUltrametricDist X]
 
-def balls (X : Type*) [PseudoMetricSpace X] : Set (Set X) := {b | ∃ x r, 0 < r ∧ b = ball x r}
+def balls (X : Type*) [PseudoMetricSpace X] : Set (Set X) := {b | ∃ x, ∃ r > 0, b = ball x r}
 
 lemma metric_space_topological_basis {X : Type*} [PseudoMetricSpace X] :
     IsTopologicalBasis (balls X) := by
@@ -21,42 +21,36 @@ lemma open_eq_union_ball {X : Type*} [PseudoMetricSpace X] (O : Set X) (hO : IsO
     ∃ s ⊆ balls X, O = ⋃₀ s :=
   IsTopologicalBasis.open_eq_sUnion metric_space_topological_basis hO
 
-def rel (U : Set (Set X)) (u v : Set X) : Prop := ∃ w ∈ balls X ∩ U, u ⊆ w ∧ v ⊆ w
+def rel (U : Set (balls X)) (u v : U) : Prop := ∃ w : U, u.1.1 ⊆ w ∧ v.1.1 ⊆ w
 
 -- Transitivite vient de la distance ultrametrique
-lemma rel_equiv (U : Set (Set X)) :
-    Equivalence (fun (u v : {b ∈ balls X | b ∈ U}) => rel U u v) where
+lemma rel_equiv {U : Set (balls X)} : Equivalence (rel U) where
   refl s := ⟨s, by simp⟩
-  symm {u v} huv := by
-    obtain ⟨w, hwU, huw, hvw⟩ := huv
-    exact ⟨w, hwU, hvw, huw⟩
-  trans := by
-    rintro ⟨u, ⟨⟨xu, ru, ru_pos, rfl⟩, u_in_U⟩⟩
-    rintro ⟨v, ⟨⟨xv, rv, rv_pos, rfl⟩, v_in_U⟩⟩
-    rintro ⟨w, ⟨⟨xw, rw, rw_pos, rfl⟩, w_in_U⟩⟩
-    rintro ⟨s, ⟨⟨xs, rs, rs_pos, rfl⟩, hsU⟩, u_sub_s, v_sub_s⟩
-    rintro ⟨t, ⟨⟨xt, rt, rt_pos, rfl⟩, htU⟩, v_sub_t, w_sub_t⟩
+  symm {u v} := by
+    rintro ⟨w, hw⟩
+    exact ⟨w, hw.symm⟩
+  trans {u v w} := by
+    rintro ⟨s, u_sub_s, v_sub_s⟩ ⟨t, v_sub_t, w_sub_t⟩
+    have : ¬ Disjoint s.1.1 t.1.1 := by
+      apply Nonempty.not_disjoint
+      rcases v with ⟨⟨v, xv, rv, rv_pos, rfl⟩, hvU⟩
+      have : xv ∈ ball xv rv := mem_ball_self rv_pos
+      refine ⟨xv, v_sub_s this, v_sub_t this⟩
+    have key : s.1.1 ⊆ t.1.1 ∨ t.1.1 ⊆ s.1.1 ∨ Disjoint s.1.1 t.1.1 := by
+      obtain ⟨⟨s, xs, rs, rs_pos, rfl⟩, hsU⟩ := s
+      obtain ⟨⟨t, xt, rt, rt_pos, rfl⟩, htU⟩ := t
+      apply IsUltrametricDist.ball_subset_trichotomy
+    rcases key with s_sub | t_sub | dis
+    · exact ⟨t, u_sub_s.trans s_sub, w_sub_t⟩
+    · exact ⟨s, u_sub_s, w_sub_t.trans t_sub⟩
+    · contradiction
 
-    have v_sub_inter : ball xv rv ⊆ ball xs rs ∩ ball xt rt := by
-      intro z hz
-      exact ⟨v_sub_s hz, v_sub_t hz⟩
-    have inter_nonempty : Set.Nonempty (ball xs rs ∩ ball xt rt) := by
-      refine ⟨xv, v_sub_s (mem_ball_self rv_pos), v_sub_t (mem_ball_self rv_pos)⟩
-
-    rcases IsUltrametricDist.ball_subset_trichotomy xs xt rs rt with s_sub | t_sub | dis
-    · exact ⟨ball xt rt, ⟨⟨xt, rt, rt_pos, rfl⟩, htU⟩, subset_trans u_sub_s s_sub, w_sub_t⟩
-    · exact ⟨ball xs rs, ⟨⟨xs, rs, rs_pos, rfl⟩, hsU⟩, u_sub_s, subset_trans w_sub_t t_sub⟩
-    · rw [disjoint_iff_inter_eq_empty] at dis
-      simp [dis] at inter_nonempty
-
-instance balls_U (U : Set (Set X)) : Setoid {b ∈ balls X | b ∈ U} where
-  r := fun u v => rel U u v
-  iseqv := rel_equiv U
+instance balls_U (U : Set (balls X)) : Setoid U := ⟨rel U, rel_equiv⟩
 
 -- def Urepr (U : Set (Set X)) : Set (Set X) := {x | ∃ (q : Quotient (balls_U U)), Quotient.out q = x}
 
-lemma has_max (U : Set (Set X)) (u : Quotient (balls_U U)) :
-    ∃ v : {b ∈ balls X | b ∈ U}, Maximal (fun w => ⟦w⟧ = u) v := sorry
+lemma has_max (U : Set (balls X)) (u : Quotient (balls_U U)) :
+    ∃ v : U, Maximal (fun w => ⟦w⟧ = u) v := sorry
   -- TODO : Verfier que c'est vrai
   -- Piste : Lemme de Zorn
 
